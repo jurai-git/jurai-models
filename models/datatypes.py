88554a7ccc6602882ad8b813b7679b1bc41c2059
wendelfrota <wendelalves898@gmail.com>
2024-08-04 01:01:16 -0300

    add: model specification data type

--- models/datatypes.py
+++ models/datatypes.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from pydantic import BaseModel
+from dataclasses import dataclass
+from typing import Optional, Dict, Any
 from strong_typing.schema import json_schema_type
 
 
@@ -8,3 +11,20 @@ class CoreModelId(Enum):
     jurai_probability = 'JurAI-Probability-1.0'
     jurai_arguments = 'JurAI-Arguments-1.0'
     jurai_text_generation = 'JurAI-Text-Generation-1.0'
+
+
+@json_schema_type
+class Model(BaseModel):
+    core_model_id: CoreModelId
+    is_default_variant: bool
+    max_seq_length: int
+    model_args: Dict[str, Any]
+
+
+@dataclass
+class ModelArgs:
+    dim: int = 256
+    n_layers: int = 6
+    vocab_size: int = 90_000
+    max_batch_size: int = 48
+    max_seq_len: int = 2048
