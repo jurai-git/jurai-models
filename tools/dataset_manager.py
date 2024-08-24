0d8cf6730b152d3746f31fbe98188d7412c8e03b
wendelfrota <wendelalves898@gmail.com>
2024-08-24 12:48:22 -0300

    remove: DatasetStages

--- tools/dataset_manager.py
+++ tools/dataset_manager.py
@@ -1,11 +1,7 @@
-import json
 import os
-import re
-
 import pandas as pd
 from dotenv import load_dotenv
 from enum import Enum
-from typing import Optional
 from strong_typing.schema import json_schema_type
 
 load_dotenv()
@@ -14,7 +10,6 @@ load_dotenv()
 class ProjectPaths(Enum):
     PROJECT_PATH = os.getenv('PROJECT_PATH')
     DATASET_PATH = os.getenv('DATASET_PATH')
-    DATASET_FILE = os.path.join(DATASET_PATH, '{}_dataset{}.csv')
     DATASET_LOGS = os.path.join(DATASET_PATH, 'logs')
 
     @staticmethod
@@ -22,15 +17,6 @@ class ProjectPaths(Enum):
         for path in ProjectPaths:
             if not path.value:
                 raise ValueError(f'{path.name} is not set or invalid.')
-
-
-class DatasetStages(Enum):
-    RAW_TYPE = 'raw'
-    CLEANED_TYPE = 'cleaned'
-    NORMALIZED_TYPE = 'normalized'
-    STANDARDIZED_TYPE = 'standardized'
-    TOKENIZED_TYPE = 'tokenized'
-    PERSONALIZED_TYPE = 'personalized'
 
 
 @json_schema_type
@@ -50,13 +36,11 @@ class DatasetManager:
         if not os.path.exists(path):
             raise FileNotFoundError(f'{path} is not a valid path.')
 
-    def read_dataset(self, dataset_stage: DatasetStages, version: Optional[str] = None, usecols=None) -> pd.DataFrame:
-        self.__raise_invalid_dataset_stage(dataset_stage)
+    def read_dataset(self, dataset, usecols=None) -> pd.DataFrame:
+        dataset = os.path.join(self.__project_paths.DATASET_PATH.value, dataset)
 
-        version = self.__validate_version(version)
-        file = self.__project_paths.DATASET_FILE.value.format(dataset_stage.value, version)
-
-        return pd.read_csv(file, usecols=usecols)
+        self.raise_path_not_found(dataset)
+        return pd.read_csv(dataset, usecols=usecols)
 
     def find_datasets(self):
         return list(os.walk(ProjectPaths.DATASET_PATH.value))[0][2]
@@ -66,19 +50,15 @@ class DatasetManager:
 
         if model is not None:
             path = os.path.join(path, model)
-
         self.raise_path_not_found(path)
 
         return list(os.walk(path))[0][2]
 
-    def save_dataset(self, df: pd.DataFrame, dataset_stage: DatasetStages, version: Optional[str] = None):
+    def save_dataset(self, df: pd.DataFrame, name: str):
         self.__raise_invalid_df(df)
-        self.__raise_invalid_dataset_stage(dataset_stage)
+        dataset = os.path.join(self.__project_paths.DATASET_PATH.value, name)
 
-        version = self.__validate_version(version)
-        dataset_file = self.__project_paths.DATASET_FILE.value.format(dataset_stage.value, version)
-
-        df.to_csv(dataset_file, index=False, encoding='utf-8')
+        df.to_csv(dataset, index=False, encoding='utf-8')
 
     def save_training_history(self, model, history, model_name):
         file = 'probability_model.log'
@@ -117,12 +97,3 @@ class DatasetManager:
     def __raise_invalid_df(self, df: pd.DataFrame):
         if df is None or df.empty:
             raise ValueError('Invalid DataFrame.')
-
-    def __raise_invalid_dataset_stage(self, dataset_stage):
-        if not dataset_stage and not isinstance(dataset_stage.value, str):
-            raise ValueError('Invalid dataset stage.')
-
-    def __validate_version(self, version):
-        if isinstance(version, str) and version.strip():
-            return f'_{version.strip()}'
-        return ''
